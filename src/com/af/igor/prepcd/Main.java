15e82043dc3765c86816070695f812339eba202e
Igor Krasylivskyi <tranzistor_2000@ukr.net>
2016-05-30 14:30:58 +0300

    Started write program. Writed comment in Main.java

---
+++ src/com/af/igor/prepcd/Main.java
@@ -0,0 +1,19 @@
+package com.af.igor.prepcd;
+
+/* This program copy filtered .pdf files to cd's plan directory.
+I..., E..., FS... plans rename to I,E,FS    M...-... -> M...pdf se, refr, hpet
+First argument is a source directory path, second - target directory path. If argument only one it is a target path,
+source path is a current directory. If there is not args, source path is a current directory and
+target path is an appropriate cd directory.
+
+else it can rename base .ckd to machine files, alike I...ckd -> I5500-GA1881.ckd
+автоматичне створення каталога диску з подальшим копіюванням html файлів, файлів креслень, розпарсювання .xls файла
+і автоматичного виделення зайвих директорій, відкриття пдф'ів інсттрукцій для ручного додаваня самих інструкцій.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
