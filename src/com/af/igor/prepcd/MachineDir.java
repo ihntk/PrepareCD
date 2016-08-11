b184a3ceaea42f4c93dba1fad8f29b83d1de96e7
Igor Krasylivskyi <tranzistor_2000@ukr.net>
2016-08-11 18:03:58 +0300

    Added code for get file lists

--- src/com/af/igor/prepcd/MachineDir.java
+++ src/com/af/igor/prepcd/MachineDir.java
@@ -1,16 +1,45 @@
 package com.af.igor.prepcd;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ede on 11.08.2016.
  */
 public class MachineDir extends Machine{
-    private String machinePathDir;
+
+    private File machinePathDir;
     final String MACHINEXLS=super.getMachineName()+".xlsx";
 
 
     public MachineDir(String machinePathDir) {
-        this.machinePathDir = machinePathDir;
+        this.machinePathDir = new File(machinePathDir);
     }
 
+    private File[] getFiles(){
+        return machinePathDir.listFiles();
 
+        List<String> pdf=new ArrayList<>();
+        List<String> ckd=new ArrayList<>();
+        for (String file:files) {
+            if (file.contains(".xls")) xls.add(file);
+            if (file.contains(".pdf")) pdf.add(file);
+            if (file.contains(".ckd")) ckd.add(file);
+        }
+    }
+
+    public ArrayList<String>getPdfFiles(){
+        return ;
+    }
+
+    public ArrayList<String>getXlsFiles(){
+        List<File> xls=new ArrayList<>();
+        for (File file:getFiles()) {
+            if (file.contains(".xls")) xls.add(file);
+        }
+        }
+    public ArrayList<String>getSkdFiles(){
+
+    }
 }
