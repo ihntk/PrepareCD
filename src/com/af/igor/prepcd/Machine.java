7132c43a83769828ea166034609736125a504651
Igor Krasylivskyi <tranzistor_2000@urk.net>
2016-08-15 22:43:57 +0300

    Added machineSeries var to Machine class

--- src/com/af/igor/prepcd/Machine.java
+++ src/com/af/igor/prepcd/Machine.java
@@ -6,18 +6,21 @@ package com.af.igor.prepcd;
 public class Machine {
     private String machineName;
     private String machineType;
+    private String machineSeries;
 
     MachineDir machineDir;
     CdDir cdDir;
 
     public Machine(String machineName) {
         this.machineName = machineName;
+        machineSeries=machineName.substring(2,4);
     }
 
     public Machine(String machineName, String machinePathDir, String cdPathDir) {
         this.machineName = machineName;
         this.machineDir = new MachineDir(machinePathDir);
         this.cdDir = new CdDir(cdPathDir);
+        machineSeries=machineName.substring(2,4);
     }
 
     public Machine() {
@@ -25,5 +28,9 @@ public class Machine {
 
     public String getMachineName() {
         return machineName;
+    }
+
+    public String getMachineSeries() {
+        return machineSeries;
     }
 }
