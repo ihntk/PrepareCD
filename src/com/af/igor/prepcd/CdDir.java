f8135eda76245759bb26c65c8ae93bbc515e9401
Igor Krasylivskyi <tranzistor_2000@urk.net>
2016-08-18 15:58:51 +0300

    Added app var and prepareCD method to CdDir

--- src/com/af/igor/prepcd/CdDir.java
+++ src/com/af/igor/prepcd/CdDir.java
@@ -4,9 +4,14 @@ package com.af.igor.prepcd;
  * Created by ede on 11.08.2016.
  */
 public class CdDir {
+    MainApp app=MainApp.getInstance();
     String cdPathDir;
 
     public CdDir(String cdPathDir) {
         this.cdPathDir = cdPathDir;
+    }
+
+    public void prepareCd() {
+
     }
 }
