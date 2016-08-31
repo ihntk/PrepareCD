74598ad0f994ee8df24c591d6cb5110525cecdb7
Igor Krasylivskyi <tranzistor_2000@ukr.net>
2016-08-31 14:00:37 +0300

    Added singleton pattern

--- src/main/java/com/af/igor/prepcd/util/ExcelParser.java
+++ src/main/java/com/af/igor/prepcd/util/ExcelParser.java
@@ -4,4 +4,13 @@ package com.af.igor.prepcd.util;
  * Created by ede on 30.08.2016.
  */
 public class ExcelParser {
+    private static ExcelParser instance;
+
+    private ExcelParser() {}
+
+    public static ExcelParser getinstance() {
+        if (instance==null)
+            instance=new ExcelParser();
+        return instance;
+    }
 }
