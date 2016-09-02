323e20a037f25f453744ddb2d74629675e7b56d5
Igor Krasylivskyi <tranzistor_2000@ukr.net>
2016-09-02 21:08:06 +0300

    Changed ExcelParser class to abstract

--- src/main/java/com/af/igor/prepcd/util/ExcelParser.java
+++ src/main/java/com/af/igor/prepcd/util/ExcelParser.java
@@ -1,16 +1,30 @@
 package com.af.igor.prepcd.util;
 
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * Created by ede on 30.08.2016.
  */
-public class ExcelParser {
-    private static ExcelParser instance;
+public abstract class ExcelParser {
+    protected String excelFile;
+    protected XSSFWorkbook workbook;
 
-    private ExcelParser() {}
-
-    public static ExcelParser getinstance() {
-        if (instance==null)
-            instance=new ExcelParser();
-        return instance;
+    public void setExcelFile(String excelFile) {
+        this.excelFile= excelFile;
+        try(FileInputStream file=new FileInputStream(excelFile)) {
+            workbook=new XSSFWorkbook(excelFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+
+    public abstract String[] getLanguage(String excelFile);
+
 }
