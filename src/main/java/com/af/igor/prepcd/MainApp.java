5685cfb667878aea6b4f576c1d3e9219bc17d829
Igor Krasylivskyi <tranzistor_2000@ukr.net>
2016-09-08 21:33:32 +0300

    Chenged version 0.2.1

--- src/main/java/com/af/igor/prepcd/MainApp.java
+++ src/main/java/com/af/igor/prepcd/MainApp.java
@@ -26,7 +26,7 @@ import java.nio.file.Paths;
 
 public class MainApp {
     private static MainApp instance;
-    private static String version = "0.2.0";
+    private static String version = "0.2.1";
     private static Machine machine;         //in future this field will replace static ArrayList<Machine>
     static final String XLS = "d:\\my_docs\\workDir\\XL's\\20XX0000.xlsx";
     static final String MACHINES = "d:\\my_docs\\plans\\";
