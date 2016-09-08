e95df0922882204c557e1ddb5fffb1c2fdc4a7b6
Igor Krasylivskyi <tranzistor_2000@ukr.net>
2016-09-08 11:54:28 +0300

    Added machine types-codes

--- src/main/java/com/af/igor/prepcd/util/MachinesCode.java
+++ src/main/java/com/af/igor/prepcd/util/MachinesCode.java
@@ -4,10 +4,35 @@ package com.af.igor.prepcd.util;
  * Created by igor on 03.09.16.
  */
 public enum MachinesCode {
-    _46XS(3000),
-    _46S(3500),
-    _46A(4000),
-    _46B(4500);
+    MC2(700),
+    CE2A(1000),
+    CE2B(1500),
+    CE24A(2000),
+    CE24B(2500),
+    CE46XS(3000),
+    L4(3050),
+    CE46S(3500),
+    L4A(3550),
+    CE46A(4000),
+    L5(4050),
+    CE46B(4500),
+    L5A(4550),
+    CE6(5000),
+    L6(5050),
+    CE68A(5500),
+    L6A(5525),
+    L6B(5550),
+    CE68S(6500),
+    CE68B(7000),
+    CE680A(7500),
+    CE680B(8000),
+    OPC55(10500),
+    OPC75(11000),
+    OPC90(11500),
+    OPC110(12000),
+    OPC132(12500),
+    OPC160(13000),
+    OPC200(13500);
 
 
     private String code;
