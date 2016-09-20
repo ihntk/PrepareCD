c64eecd355ebfb8dbd9e2ba0d0765b8e867f4ff6
Igor Krasylivskyi <tranzistor_2000@ukr.net>
2016-09-20 13:14:02 +0300

    Added date of starting logging

--- src/main/java/com/af/igor/prepcd/log/SimpleLogger.java
+++ src/main/java/com/af/igor/prepcd/log/SimpleLogger.java
@@ -2,22 +2,26 @@ package com.af.igor.prepcd.log;
 
 import com.af.igor.prepcd.MainApp;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by ede on 19.09.2016.
  */
 public class SimpleLogger {
     private FileOutputStream outputStream;
-    private MainApp app=MainApp.getInstance();
+    private MainApp app = MainApp.getInstance();
 
     public SimpleLogger() {
+        Date date = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        String time = dateFormat.format(date);
         try {
-            outputStream=new FileOutputStream(app.LOGFILE, true);
-            log("=====\nProgram started\n");
+            outputStream = new FileOutputStream(app.LOGFILE, true);
+            log("=====\nProgram started at " + time + "\n");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -25,13 +29,13 @@ public class SimpleLogger {
 
     public void log(String message) {
         try {
-            outputStream.write((message+"\n").getBytes());
+            outputStream.write((message + "\n").getBytes());
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void stopLogging(){
+    public void stopLogging() {
         try {
             log("\nProgram finished\n-----\n\n");
             outputStream.close();
