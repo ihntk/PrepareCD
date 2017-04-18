7645d38ce7368c2169e81e767607d17c334cc24d
Igor Krasylivskyi <tranzistor_2000@ukr.net>
2017-04-18 10:57:53 +0300

    Added test for MainApp

---
+++ src/test/java/com/af/igor/prepcd/MainAppTest.java
@@ -0,0 +1,11 @@
+package com.af.igor.prepcd;
+
+/**
+ * Created by ede on 18.04.2017.
+ */
+public class MainAppTest {
+    private static MainApp mainApp;
+    public static void main(String[] args) {
+        mainApp =MainApp.getInstance();
+    }
+}
