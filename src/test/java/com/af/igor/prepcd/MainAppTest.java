774e09de62c03dceacf444f282e04e4aed1e163d
Igor Krasylivskyi <tranzistor_2000@ukr.net>
2017-04-18 12:22:49 +0300

    Added testPath() to MainAppTest

--- src/test/java/com/af/igor/prepcd/MainAppTest.java
+++ src/test/java/com/af/igor/prepcd/MainAppTest.java
@@ -1,11 +1,26 @@
 package com.af.igor.prepcd;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by ede on 18.04.2017.
  */
 public class MainAppTest {
     private static MainApp mainApp;
-    public static void main(String[] args) {
+    private static Desktop desktop=Desktop.getDesktop();
+
+    public static void main(String[] args) throws IOException {
         mainApp =MainApp.getInstance();
+        testPath();
+    }
+
+    private static void testPath() throws IOException {
+        desktop.open(new File(mainApp.LOGFILE));
+        mainApp.tc(mainApp.LUX_DIR);
+        desktop.open(new File(mainApp.LUX_DIR));
+        mainApp.tc(mainApp.MACHINES);
+        desktop.open(new File(mainApp.XLS));
     }
 }
