cf478cadaa4d224a1a32c67ac8d3ec03346d63bb
Igor Krasylivskyi <tranzistor_2000@ukr.net>
2017-04-26 13:18:09 +0300

    Moved sheet from methods to class field;

--- src/main/java/com/af/igor/prepcd/util/MachineExcelParser.java
+++ src/main/java/com/af/igor/prepcd/util/MachineExcelParser.java
@@ -10,6 +10,13 @@ public class MachineExcelParser extends ExcelParser{
     private static MachineExcelParser instance;
 
     private MachineExcelParser() {}
+    XSSFSheet sheet;
+
+    @Override
+    public void setExcelFile(String excelFile) {
+        super.setExcelFile(excelFile);
+        sheet=workbook.getSheetAt(0);
+    }
 
     public static MachineExcelParser getInstance() {
         if (instance==null)
@@ -18,14 +25,12 @@ public class MachineExcelParser extends ExcelParser{
     }
 
     public String[] getLanguage() {
-        XSSFSheet sheet=workbook.getSheetAt(0);
         String languages=String.format("%8s",sheet.getRow(0).getCell(0));
         languages=languages.substring(languages.indexOf("CD")+3,languages.indexOf("suv")).trim();
         return languages.split("/");
     }
 
     public String getMPlans() {
-        XSSFSheet sheet=workbook.getSheetAt(0);
         String mPlans=String.format("%s",sheet.getRow(19).getCell(7));
         return mPlans;
     }
