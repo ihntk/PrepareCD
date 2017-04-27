370286fcdb2ac1b184d6818597c75a2309713f6e
Igor Krasylivskyi <tranzistor_2000@ukr.net>
2017-04-27 13:58:17 +0300

    Modified setMachineType()

--- src/main/java/com/af/igor/prepcd/util/MachineExcelParser.java
+++ src/main/java/com/af/igor/prepcd/util/MachineExcelParser.java
@@ -1,41 +1,43 @@
 package com.af.igor.prepcd.util;
 
 import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
  * Created by ede on 30.08.2016.
  */
-public class MachineExcelParser extends ExcelParser{
+public class MachineExcelParser extends ExcelParser {
     private static MachineExcelParser instance;
 
-    private MachineExcelParser() {}
+    private MachineExcelParser() {
+    }
+
     XSSFSheet sheet;
 
     @Override
     public void setExcelFile(String excelFile) {
         super.setExcelFile(excelFile);
-        sheet=workbook.getSheetAt(0);
+        sheet = workbook.getSheetAt(0);
     }
 
     public static MachineExcelParser getInstance() {
-        if (instance==null)
-            instance=new MachineExcelParser();
+        if (instance == null)
+            instance = new MachineExcelParser();
         return instance;
     }
 
     public String[] getLanguage() {
-        String languages=String.format("%8s",sheet.getRow(0).getCell(0));
-        languages=languages.substring(languages.indexOf("CD")+3,languages.indexOf("suv")).trim();
+        String languages = String.format("%8s", sheet.getRow(0).getCell(0));
+        languages = languages.substring(languages.indexOf("CD") + 3, languages.indexOf("suv")).trim();
         return languages.split("/");
     }
 
     public String getMPlans() {
-        String mPlans=String.format("%s",sheet.getRow(19).getCell(7));
+        String mPlans = String.format("%s", sheet.getRow(19).getCell(7));
         return mPlans;
     }
 
     public void setMachineType(String machineType) {
-        sheet.getRow(2).getCell(1).setCellValue(machineType);
+        sheet.createRow(4).createCell(7).setCellValue(machineType);
+        writeXLS();
     }
 }
