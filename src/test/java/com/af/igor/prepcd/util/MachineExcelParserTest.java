34461ebca710789bae350a2937d0e6ab9def3ecf
Igor Krasylivskyi <tranzistor_2000@ukr.net>
2017-04-27 13:59:40 +0300

    Added MachineExcelParserTest

---
+++ src/test/java/com/af/igor/prepcd/util/MachineExcelParserTest.java
@@ -0,0 +1,17 @@
+package com.af.igor.prepcd.util;
+
+/**
+ * Created by ede on 26.04.2017.
+ */
+public class MachineExcelParserTest {
+    private static MachineExcelParser machineExcelParser;
+    private static String excelPath="d:\\my_docs\\workDir\\20FF0000.xlsx";
+    private static String machineName="20JN2354";
+    private static String machineType="L6A";
+
+    public static void main(String[] args) {
+        machineExcelParser=MachineExcelParser.getInstance();
+        machineExcelParser.setExcelFile(excelPath);
+        machineExcelParser.setMachineType(machineType);
+    }
+}
