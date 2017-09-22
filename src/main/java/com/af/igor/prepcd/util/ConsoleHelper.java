8ee83f032dab9ba3387264f3bc0d0d0c526bb902
Igor Krasylivskyi <tranzistor_2000@ukr.net>
2017-09-22 15:49:02 +0300

    Added ConsoleHelper class

---
+++ src/main/java/com/af/igor/prepcd/util/ConsoleHelper.java
@@ -0,0 +1,27 @@
+package com.af.igor.prepcd.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleHelper {
+    private static BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+
+    public static void WriteMessage(String message){
+        System.out.println(message);
+    }
+
+    public static String readString(){
+        String line = null;
+
+        do {
+            try {
+                line = reader.readLine();
+            } catch (IOException e) {
+                System.out.println("Try again");
+            }
+        }while (line==null);
+
+        return line;
+    }
+}
