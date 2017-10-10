a578414acb8debe5aa9f463c1328c915abf1a655
Igor Krasylivskyi <tranzistor_2000@ukr.net>
2017-10-10 18:20:06 +0300

    Added mainFrame to PrepareCD class

--- src/main/java/com/af/igor/prepcd/PrepareCD.java
+++ src/main/java/com/af/igor/prepcd/PrepareCD.java
@@ -1,9 +1,17 @@
 package com.af.igor.prepcd;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class PrepareCD extends Application {
+    private Stage primaryStage;
+    private VBox rootLayout;
 
     public static void main(String[] args) {
         launch(args);
@@ -11,6 +19,39 @@ public class PrepareCD extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        primaryStage.setTitle("PrepareCD");
 
+        initRootLayout();
+
+        showMainFrame();
+    }
+
+    public void initRootLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/view/RootLayout.fxml"));
+            rootLayout = (VBox) loader.load();
+            Scene scene = new Scene(rootLayout);
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showMainFrame() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/view/MainFrame.fxml"));
+            BorderPane mainFrame = (BorderPane) loader.load();
+
+            rootLayout.getChildren().add(mainFrame);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
