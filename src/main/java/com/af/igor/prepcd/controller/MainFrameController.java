0aba11b7483faafc48103b04093c4177e2848059
Igor Krasylivskyi <tranzistor_2000@ukr.net>
2017-10-11 09:15:22 +0300

    Added MainFrameController

---
+++ src/main/java/com/af/igor/prepcd/controller/MainFrameController.java
@@ -0,0 +1,44 @@
+package com.af.igor.prepcd.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class MainFrameController {
+    @FXML
+    private Label machine;
+
+    @FXML
+    private Label machineType;
+
+    @FXML
+    private Label status;
+
+    @FXML
+    private TextField machineName;
+
+    @FXML
+    private TextField machineCode;
+
+    @FXML
+    private ListView<String> machineDir;
+
+    @FXML
+    private ListView<String> remoteMachineDir;
+
+    @FXML
+    private ListView<String> basePlanDir;
+
+    @FXML
+    private ListView<String> cdDir;
+
+
+    public MainFrameController() {
+    }
+
+    public void initialize(){
+        machineName.setText("20GA2869");
+        machineCode.setText("5000");
+    }
+}
