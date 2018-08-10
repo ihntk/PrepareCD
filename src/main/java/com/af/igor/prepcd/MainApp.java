e1816cd4f82f830b4b58da5bc20dfa7fb4da2d27
Igor Krasylivskyi <tranzistor_2000@ukr.net>
2018-08-10 15:53:00 +0300

    Changed version 0.6.0.a03

--- src/main/java/com/af/igor/prepcd/MainApp.java
+++ src/main/java/com/af/igor/prepcd/MainApp.java
@@ -38,7 +38,7 @@ public class MainApp {
     static String TOTALCOMMANDER;
 
     private static MainApp instance;
-    private static String version = "0.6.0.a03";
+    private static String version = "0.6.0.a04";
     private static Machine machine;         //in future this field will replace static ArrayList<Machine>
     static public LuxParser luxParser;
     static MachineExcelParser machineExcelParser;
