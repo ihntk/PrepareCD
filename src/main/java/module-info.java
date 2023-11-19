d4f8288aa97a1d349d8aeedfa21a36c1fbc358c8
Ihor Krasylivskyi <tranzistor_2000@ukr.net>
2023-11-19 20:44:43 +0200

    Updated ideas files

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.af.igor.prepcd.preparecd {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.desktop;
+    requires java.prefs;
+    requires org.apache.poi.poi;
+    requires org.apache.poi.ooxml;
+    requires org.apache.commons.collections4;
+
+    opens com.af.igor.prepcd to javafx.fxml;
+    exports com.af.igor.prepcd;
+    opens com.af.igor.prepcd.controller;
+    exports com.af.igor.prepcd.controller;
+}
